9d6883d4c428923125f02e8f108d1b83c8d0050d
Magdy <abdelrhmanmagdy582@gmail.com>
2023-07-17 13:10:52 -0700

    a

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: A
+ *
+ * Return: Alawys 0 (Success)
+*/
+
+int main(void)
+{
+	int digit;
+
+	while (digit <= 102)
+	{
+		putchar(digit + '0');
+		if (digit == 57)
+			digit += 39;
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}
