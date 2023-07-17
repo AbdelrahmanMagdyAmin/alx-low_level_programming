d1c25165f1f15cd2d3cdd8efb8efa21bbdc16792
Magdy <abdelrhmanmagdy582@gmail.com>
2023-07-17 12:55:23 -0700

    a

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - Entry point
+ *
+ * Description: A
+ *
+ * Return: Alawys 0 (Success)
+*/
+
+int main(void)
+{
+	for (int i = 0 ; i < 10 ; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+	return (0);
+}
