e25da0b632c9f6d41d07ea4075618ce3a4bfaa18
Magdy <abdelrhmanmagdy582@gmail.com>
2023-07-18 11:25:48 -0700

    a

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_to_98 - function to print from n to 98
+ *
+ * @n: takes input from user
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+}
+
+
