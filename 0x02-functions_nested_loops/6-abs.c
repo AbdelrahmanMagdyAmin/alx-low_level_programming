fc0ddd03e7508e271e0d3816d5bad875fd8ad902
Magdy <abdelrhmanmagdy582@gmail.com>
2023-07-18 10:13:12 -0700

    a

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - function to print absolute of number
+ *
+ * @n: take input from user
+ *
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n *= -1;
+	return (n);
+}
