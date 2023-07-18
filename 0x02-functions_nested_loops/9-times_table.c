dce9ea605c22e5593edb95f7ec8373c54cc50ce1
Magdy <abdelrhmanmagdy582@gmail.com>
2023-07-18 10:44:37 -0700

    a

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * times_table - function to print time table
+ */
+void times_table(void)
+{
+	int n, i, r;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (i = 0; i <= 9; i++)
+		{
+			r = n * i;
+			_putchar(r + 48);
+		}
+		_putchar('\n');
+	}
+}
+
