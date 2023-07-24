75dc492bdd915688589d8ad79338c404bb1d4b6a
Magdy <abdelrhmanmagdy582@gmail.com>
2023-07-24 13:26:10 -0700

    a

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_array  - takes array and number of elements and print elemnts
+ *
+ * @a: int paramter
+ * @n: int paramter
+ *
+ * Return: Nothing
+*/
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d, ", a[i];
+	}
+}
