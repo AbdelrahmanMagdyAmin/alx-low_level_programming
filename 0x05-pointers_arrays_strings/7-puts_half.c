2e5f83e2fb5a4354241595f867f3ec12c0ab2238
Magdy <abdelrhmanmagdy582@gmail.com>
2023-07-24 13:11:07 -0700

    a

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half  - takes string and print second half of string
+ *
+ * @str: char paramter
+ *
+ * Return: Nothing
+*/
+void puts_half(char *str)
+{
+	int i = 0, n = 0;
+	
+	while (str[i] != '\0')
+		i++;
+	n = (i - 1) / 2;
+	while (n < i)
+	{
+		printf("%c", str[n]);
+		n++;
+	}
+}	
