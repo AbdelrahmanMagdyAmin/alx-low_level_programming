e04da40486d566677fbad33f9c22d6320cf7f16a
Magdy <abdelrhmanmagdy582@gmail.com>
2023-07-24 11:44:23 -0700

    a

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - it print string followed by new line
+ *
+ * @str: char paramter
+ *
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		printf("%c",str[i]);
+		i++;
+	}
+	putchar('\n');
+}
