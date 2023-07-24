1f063767dd1f1123a46c8e273cbe504a1f9dc65a
Magdy <abdelrhmanmagdy582@gmail.com>
2023-07-24 12:51:50 -0700

    a

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2  - takes string and print each char in  string
+ *
+ * @str: char paramter
+ *
+ * Return: Nothing
+*/
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		printf("%c\n", str[i]);
+	}
+}	
